6d9fa8ecc41e0010e453d2e5c64537425499a39a
Brian D. Winters <brian@6by9.us>
2024-03-24 14:48:54 -0700

    Easy checks for which mpmath backend library is being used. Not really necessary, but they are a reminder for me that this is a thing.

--- astroalg/util.py
+++ astroalg/util.py
@@ -4,8 +4,23 @@ Calculations from Astronomical Algorithms by Jean Meeus, second edition
 Utility functions
 '''
 
+import mpmath.libmp
 from mpmath import radians
+
 
 def degrees_to_radians(deg):
     'Convert deg to radians, first by adjusting deg ∈ [0..360)'
     return radians(deg % 360)
+
+
+def mpmath_using_python() -> bool:
+    'Check whether mpmath is using the python backend'
+    return mpmath.libmp.BACKEND == 'python'
+
+
+def mpmath_using_gmp() -> bool:
+    '''
+    Check whether mpmath is using the gmpy backend.
+    gmpy is the Python wrapper around GMP.
+    '''
+    return mpmath.libmp.BACKEND == 'gmpy'
